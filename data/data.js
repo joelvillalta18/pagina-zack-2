8d6e4ba498594105d009c0b766d10c9f17e7ffbb
joel villalta <giancarlos64a@gmail.com>
2023-11-15 15:54:38 -0500

    cambios hoy

---
+++ data/data.js
@@ -0,0 +1,54 @@
+export const socialNetworks = [
+  {
+    id: 1,
+    href: 'https://www.youtube.com/channel/UCF2B4tXgHUoPKxhRMahbwrw',
+    title: 'Red social de YouTube',
+    content: '<i class="bi bi-youtube"></i>'
+  },
+  {
+    id: 2,
+    href: 'https://github.com/gabo2666',
+    title: 'Red social de GitHub',
+    content: '<i class="bi bi-github"></i>'
+  },
+  {
+    id: 3,
+    href: 'https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer"',
+    title: 'Red social de LinkedIn',
+    content: '<i class="bi bi-linkedin"></i>'
+  },
+  {
+    id: 4,
+    href: 'https://www.instagram.com/zacklian_killer43/',
+    title: 'Red social de Instagram',
+    content: '<i class="bi bi-instagram"></i>'
+  }
+];
+
+  
+  export const teams = [
+    {
+      id: 1,
+      title: 'H07',
+      subtitle: 'CALLAO',
+      description: 'JR.ABANCAY 77 ',
+      image: 'https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/04/amigos-diversion-risas-desayuno-cafe-2680373.jpg?tf=1920x',
+      slug: './teams/h07'
+    },
+    {
+      id: 2,
+      title: 'H10',
+      subtitle: 'los olivos',
+      description: 'los olivos 43 urb. los olivos altura 30  ',
+      image: 'https://i.blogs.es/168816/videojuegos/1366_2000.jpg',
+      slug: './teams/h10'
+    },
+    {
+      id: 3,
+      title: 'P28',
+      subtitle: 'Villa el Salvador',
+      description: 'calle Y villa el salvador ',
+      image: 'https://img.freepik.com/fotos-premium/jugadores-modernos-que-transmiten-videojuegos-multijugador-linea-juegan-computadora-disfrutan-torneo-juego-disparos-streamer-femenina-jugando-competencia-juegos-rol-accion-monitor-pc_482257-47449.jpg?w=826',
+      slug: './teams/p28'
+    }
+  ];
